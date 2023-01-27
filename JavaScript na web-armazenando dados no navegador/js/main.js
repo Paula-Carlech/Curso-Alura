a4df4aab9d6d96131707e9ce82cd91917a24a227
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-01-26 21:51:07 -0300

    Create main.js

---
+++ JavaScript na web-armazenando dados no navegador/js/main.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('#novoItem');
+const lista = document.querySelector('#lista');
+const item = [];
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const nome  = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = "";
+    quantidade.value = "";
+})
+
+function criaElemento(nome, quantidade){
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    item.push(itemAtual);
+
+    localStorage.setItem("item", JSON.stringify(item));
+}
