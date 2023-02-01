6daf51f7eada40842a978c989e5bbfc8c77653f4
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-01-31 21:59:31 -0300

    Update validacao.js

--- JavaScript-validações e reconhecimento de voz/js/validacao.js
+++ JavaScript-validações e reconhecimento de voz/js/validacao.js
@@ -3,16 +3,26 @@ function verificaValidadeDoChute(chute){
 
     if(chuteInvalido(numero)){
         elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return
     }
 
     if(numeroForMaiorOuMenorQuePermitido(numero)){
         elementoChute.innerHTML += ` <div>Valor inválido: O valor deve estar entre: ${menorValor} e ${maiorValor}</div> `;
+        return
     }
 
     if(numero === numeroSecreto){
         document.body.innerHTML = `
         <h2>Você acertou!!</h2>
         <h3>O número secreto era : ${numeroSecreto}</h3>
+        `
+    }else if(numero > numeroSecreto){
+        elementoChute.innerHTML = `
+        <div>O número secreto é menor <i class="fa-sharp fa-solid fa-angle-down"></i></div>
+        `
+    }else{
+        elementoChute.innerHTML = `
+        <div>O número secreto é maior <i class="fa-sharp fa-solid fa-angle-up"></i></div>
         `
     }
 }
