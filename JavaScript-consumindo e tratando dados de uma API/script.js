2f4918ce5cd602e3c4afb96220c415584f6cc603
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-02-02 20:38:16 -0300

    Update script.js

--- JavaScript-consumindo e tratando dados de uma API/script.js
+++ JavaScript-consumindo e tratando dados de uma API/script.js
@@ -1,5 +1,13 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json')
-.then(resposta => resposta.json())
-.then(r => console.log(r));
-
-console.log(consultaCEP);
+async function buscaEndereco(cep){
+    try{
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if(consultaCEPConvertida.erro){
+        throw Error('CEP não existe!');
+    }
+    console.log(consultaCEPConvertida)
+    return consultaCEPConvertida;
+    }catch(erro){
+        console.log(erro);
+    }
+}
