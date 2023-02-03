d5b695cc9cf2213bad842d469644c07c71da64a6
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-02-03 09:41:01 -0300

    Update script.js

--- JavaScript-consumindo e tratando dados de uma API/script.js
+++ JavaScript-consumindo e tratando dados de uma API/script.js
@@ -1,13 +1,34 @@
 async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+
     try{
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
     var consultaCEPConvertida = await consultaCEP.json();
+
     if(consultaCEPConvertida.erro){
         throw Error('CEP não existe!');
     }
-    console.log(consultaCEPConvertida)
+
+    var cidade = document.getElementById('cidade');
+    var estado = document.getElementById('estado');
+    var logradouro = document.getElementById('endereco');
+    var bairro = document.getElementById('bairro');
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+    bairro.value = consultaCEPConvertida.bairro;
+
+    console.log(consultaCEPConvertida);
+
     return consultaCEPConvertida;
+
     }catch(erro){
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente.</p>`
         console.log(erro);
     }
 }
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
