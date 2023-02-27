90eadb18facbd03245e59330399973fd07ee6125
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-02-27 16:14:23 -0300

    Update script.js

--- Curso Javascript-Validando formulários/js/script.js
+++ Curso Javascript-Validando formulários/js/script.js
@@ -1,4 +1,5 @@
 import ehUmCPF from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
 const camposDoFormulario = document.querySelectorAll("[required]");
 
 console.log(camposDoFormulario);
@@ -11,4 +12,8 @@ function vefificaCampo(campo){
    if(campo.name == "cpf" && campo.value.length >= 11){
         ehUmCPF(campo);
    } 
+
+   if(campo.name == "aniversario" && campo.value != ""){
+    ehMaiorDeIdade(campo);
+   }
 }
