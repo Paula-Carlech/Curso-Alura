c9d4a5783552494d4a2fb674f08dddd1d212b07f
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2023-02-28 14:42:56 -0300

    Bug fix

--- Curso Javascript-Validando formulários/js/script.js
+++ Curso Javascript-Validando formulários/js/script.js
@@ -1,7 +1,7 @@
 import ehUmCPF from "./valida-cpf.js";
 import ehMaiorDeIdade from "./valida-idade.js";
-const camposDoFormulario = document.querySelectorAll("[required]");
-const formulario = document.querySelector("[data-formulario]");
+const camposDoFormulario = document.querySelectorAll('[required]')
+const formulario = document.querySelector('[data-formulario]');
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -16,13 +16,11 @@ formulario.addEventListener("submit", (e) => {
 
     localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
 
-    window.location.href = './abrir-conta-form-2.html';
+    window.location.href = "./abrir-conta-form-2.html";
 })
 
-console.log(camposDoFormulario);
-
 camposDoFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => vefificaCampo(campo));
+    campo.addEventListener("blur", () => verificaCampo(campo));
     campo.addEventListener("invalid", evento => evento.preventDefault())
 })
 
@@ -43,7 +41,7 @@ const mensagens = {
     email: {
         valueMissing: "O campo de e-mail não pode estar vazio.",
         typeMismatch: "Por favor, preencha um email válido.",
-        tooShort: "Por favor, preencha um e-mail válido."
+        tooShort: "Por favor, preencha um email válido."
     },
     rg: {
         valueMissing: "O campo de RG não pode estar vazio.",
@@ -65,30 +63,27 @@ const mensagens = {
     }
 }
 
-function vefificaCampo(campo){
+function verificaCampo(campo) {
     let mensagem = "";
     campo.setCustomValidity('');
-   if(campo.name == "cpf" && campo.value.length >= 11){
+    if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
-   } 
-
-   if(campo.name == "aniversario" && campo.value != ""){
-    ehMaiorDeIdade(campo);
-   }
-
-   tiposDeErro.forEach(erro => {
-    if(campo.validity[erro]){
-        mensagem = mensagens[campo.name][erro];
-        console.log(mensagem);
     }
-   })
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
+    }
+    tiposDeErro.forEach(erro => {
+        if (campo.validity[erro]) {
+            mensagem = mensagens[campo.name][erro];
+            console.log(mensagem);
+        }
+    })
+    const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
+    const validadorDeInput = campo.checkValidity();
 
-   const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
-   const validadorDeInput = campo.checkValidity();
-
-   if(!validadorDeInput){
-    mensagemErro.textContent = mensagem;
-   }else{
-    mensagemErro.textContent = "";
-   }
+    if (!validadorDeInput) {
+        mensagemErro.textContent = mensagem;
+    } else {
+        mensagemErro.textContent = "";
+    }
 }
